b815126cc6f14db6d3b0f7361ceda658922eadbe
adhamtaha <adhamtaha>
2023-08-30 12:31:45 +0200

    try to push change to see the conflict

--- Learning/main.cpp
+++ Learning/main.cpp
@@ -119,7 +119,7 @@ int main() {
 
 
 		cout << "new Branche called feature" << endl;
-		cout << "add more functionalities to feature branch" << endl;
+		cout << "today is very good weather" << endl;
 
 	return 0;
 }
